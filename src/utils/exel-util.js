f72b2cfd4da53237f34a6361e87684d93381f5a5
akbars-dev <akbarshox.coder@gmail.com>
2024-06-28 13:42:31 +0500

    Excel util bugs fixed

--- src/utils/exel-util.js
+++ src/utils/exel-util.js
@@ -11,7 +11,7 @@ async function createExelFile (data, fileName) {
         worksheet.addRow(Object.values(row));
     })
     
-    const workbook.xlsx.writeFile(fileName);
+    workbook.xlsx.writeFile(fileName);
 }
 
 module.exports = createExelFile;
