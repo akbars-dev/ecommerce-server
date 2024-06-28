8b9283892db655a105de988e954ffe3b6786b834
akbars-dev <akbarshox.coder@gmail.com>
2024-06-28 16:09:04 +0500

    Analiytics controller modifed

--- src/controllers/analiytics-controller.js
+++ src/controllers/analiytics-controller.js
@@ -7,7 +7,7 @@ class AnaliyticsController {
     async top (req, res, next) {
         try {
             const data = await analiyticsService.top();
-            return res.json({ status: 200, message: 'Statistika shu yerda' })
+            return res.json({ status: 200, message: 'Statistika shu yerda', data: data })
         } catch (e) {
             next(e);
         }
