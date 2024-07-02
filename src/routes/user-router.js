3b8e7a5d8e27e9c254b903313708807aeffbf6c1
akbars-dev <akbarshox.coder@gmail.com>
2024-07-02 15:52:41 +0500

    barcodes modifed

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -10,7 +10,7 @@ userRouter.get('/get/:id', usersController.getUser);
 userRouter.get('/all', usersController.all);
 userRouter.get('/get-telegram/:id', usersController.getUserByTelegramId);
 userRouter.get('/get-lang/:id', usersController.getUserLang);
-userRouter.get('/get-barcodes/', usersController.getUserLang);
+userRouter.get('/get-barcodes/', usersController.getAllBarcodes);
 userRouter.get('/get-tg/', usersController.getTelegramId);
 userRouter.get('/check/:id', usersController.checkUser);
 userRouter.get('/get-tg-birthday/', usersController.getTelegramIdAndBithday);
